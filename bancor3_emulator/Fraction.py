33e7b49810938979570100c2287ca0e3356af810
Barak Manos <barakman@yahoo.com>
2022-06-12 11:38:43 +0300

    Improve `Fraction.py`.

--- bancor3_emulator/Fraction.py
+++ bancor3_emulator/Fraction.py
@@ -1,11 +1,11 @@
 from Types import uint256, uint112
 
 class Fraction256:
-    def __init__(self) -> None:
-        self.n = uint256()
-        self.d = uint256()
+    def __init__(self, f = {'n': 0, 'd': 0}) -> None:
+        self.n = uint256(f['n'])
+        self.d = uint256(f['d'])
 
 class Fraction112:
-    def __init__(self) -> None:
-        self.n = uint112()
-        self.d = uint112()
+    def __init__(self, f = {'n': 0, 'd': 0}) -> None:
+        self.n = uint112(f['n'])
+        self.d = uint112(f['d'])

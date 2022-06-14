80adc4a201e9a09278e5971470e84bb26a548c19
Barak Manos <barakman@yahoo.com>
2022-06-14 14:28:48 +0300

    Add a temporary testing infrastructure for verifying the Python emulation vs Solidity.

---
+++ bancor3_emulator/temp/hardhat.config.js
@@ -0,0 +1,24 @@
+require("@nomiclabs/hardhat-truffle5");
+
+module.exports = {
+    solidity: {
+        version: "0.8.14",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 20000
+            }
+        }
+    },
+    paths: {
+        sources: "./project/contracts",
+        tests: "./project/tests",
+        cache: "./project/cache",
+        artifacts: "./project/artifacts"
+    },
+    mocha: {
+        useColors: true,
+        enableTimeouts: false,
+        reporter: "list" // https://mochajs.org/#reporters
+    }
+};

596097801e5904b9e48143b5476c177dce441be8
Barak Manos <barakman@yahoo.com>
2022-06-17 17:22:34 +0200

    Fix the `PoolCollectionWithdrawal` module and extend its testing.

--- bancor3_emulator/temp/project/tests/PoolCollectionWithdrawal.js
+++ bancor3_emulator/temp/project/tests/PoolCollectionWithdrawal.js
@@ -16,21 +16,23 @@ contract.only('PoolCollectionWithdrawal', () => {
         console.log();
 
         for (let n = 1; n <=8; n++) {
-            const filePath = path.join(__dirname, 'data', `PoolCollectionWithdrawalCoverage${n}.json`);
-            const table = JSON.parse(fs.readFileSync(filePath, { encoding: 'utf8' }));
-
+            const fileName = `PoolCollectionWithdrawalCoverage${n}`;
+            const table = JSON.parse(fs.readFileSync(path.join(__dirname, 'data', fileName + '.json'), { encoding: 'utf8' }));
             for (const { a, b, c, e, w, m, n, x } of table) {
                 const actual = await poolCollectionWithdrawal.calculateWithdrawalAmountsT(a, b, c, e, w, m, n, x);
                 const actualZp = new BN(actual.p.value).muln(actual.p.isNeg ? -1 : 1);
                 const actualZq = new BN(actual.q.value).muln(actual.q.isNeg ? -1 : 1);
                 const actualZr = new BN(actual.r.value).muln(actual.r.isNeg ? -1 : 1);
-                console.log(actualZp.toString());
-                console.log(actualZq.toString());
-                console.log(actualZr.toString());
-                console.log(actual.s.toString());
-                console.log(actual.t.toString());
-                console.log(actual.u.toString());
-                console.log(actual.v.toString());
+                console.log(
+                    fileName + ':',
+                    actualZp.toString(),
+                    actualZq.toString(),
+                    actualZr.toString(),
+                    actual.s.toString(),
+                    actual.t.toString(),
+                    actual.u.toString(),
+                    actual.v.toString()
+                );
             }
         };
     });

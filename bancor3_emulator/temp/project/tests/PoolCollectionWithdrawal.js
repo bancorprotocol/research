67484ccdc293b3e875a44b79febb043ae442e189
Barak Manos <barakman@yahoo.com>
2022-06-17 19:44:02 +0200

    Fix the `PoolCollectionWithdrawal` module's test.

--- bancor3_emulator/temp/project/tests/PoolCollectionWithdrawal.js
+++ bancor3_emulator/temp/project/tests/PoolCollectionWithdrawal.js
@@ -3,8 +3,6 @@ const PoolCollectionWithdrawal = artifacts.require("TestPoolCollectionWithdrawal
 const fs = require("fs");
 const path = require("path");
 
-const BN = web3.utils.BN;
-
 contract.only('PoolCollectionWithdrawal', () => {
     let poolCollectionWithdrawal;
 
@@ -20,18 +18,18 @@ contract.only('PoolCollectionWithdrawal', () => {
             const table = JSON.parse(fs.readFileSync(path.join(__dirname, 'data', fileName + '.json'), { encoding: 'utf8' }));
             for (const { a, b, c, e, w, m, n, x } of table) {
                 const actual = await poolCollectionWithdrawal.calculateWithdrawalAmountsT(a, b, c, e, w, m, n, x);
-                const actualZp = new BN(actual.p.value).muln(actual.p.isNeg ? -1 : 1);
-                const actualZq = new BN(actual.q.value).muln(actual.q.isNeg ? -1 : 1);
-                const actualZr = new BN(actual.r.value).muln(actual.r.isNeg ? -1 : 1);
+                const actualZp = actual.p.isNeg && actual.p.value !== '0' ? '-' + actual.p.value : actual.p.value;
+                const actualZq = actual.q.isNeg && actual.q.value !== '0' ? '-' + actual.q.value : actual.q.value;
+                const actualZr = actual.r.isNeg && actual.r.value !== '0' ? '-' + actual.r.value : actual.r.value;
                 console.log(
                     fileName + ':',
-                    actualZp.toString(),
-                    actualZq.toString(),
-                    actualZr.toString(),
-                    actual.s.toString(),
-                    actual.t.toString(),
-                    actual.u.toString(),
-                    actual.v.toString()
+                    actualZp,
+                    actualZq,
+                    actualZr,
+                    actual.s,
+                    actual.t,
+                    actual.u,
+                    actual.v
                 );
             }
         };

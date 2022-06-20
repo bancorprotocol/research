f4841dab59c3f6a1f5db2da678a4ec1ff6afb2af
Barak Manos <barakman@yahoo.com>
2022-06-20 07:59:09 +0200

    Add an emulation of the `Vault` contract.

---
+++ bancor3_emulator/Vault.py
@@ -0,0 +1,26 @@
+class Vault:
+    '''
+     * @dev a "virtual" constructor that is only used to set immutable state variables
+    '''
+    def __init__(self, initBNTGovernance, initVBNTGovernance) -> None:
+        self._bntGovernance = initBNTGovernance;
+        self._bnt = initBNTGovernance.token();
+        self._vbntGovernance = initVBNTGovernance;
+        self._vbnt = initVBNTGovernance.token();
+
+    '''
+     * @inheritdoc IVault
+    '''
+    def withdrawFunds(self, sender, token, target, amount) -> None:
+        token.transfer(sender, target, amount);
+
+    '''
+     * @inheritdoc IVault
+    '''
+    def burn(self, token, amount) -> None:
+        if (token is (self._bnt)):
+            self._bntGovernance.burn(amount);
+        elif (token is (self._vbnt)):
+            self._vbntGovernance.burn(amount);
+        else:
+            token.burn(amount);

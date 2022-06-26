af8eb2d3387b299eb3bdf0d364838ff1dee0f14f
Barak Manos <barakman@yahoo.com>
2022-06-26 13:14:30 +0200

    Add an emulation of the `PoolTokenFactory` contract.

---
+++ bancor3_emulator/PoolTokenFactory.py
@@ -0,0 +1,86 @@
+from solidity import uint, uint8, mapping
+
+from PoolToken import PoolToken
+
+'''
+ * @dev Pool Token Factory contract
+'''
+class PoolTokenFactory:
+    POOL_TOKEN_SYMBOL_PREFIX = "bn";
+    POOL_TOKEN_NAME_PREFIX = "Bancor";
+    POOL_TOKEN_NAME_SUFFIX = "Pool Token";
+
+    '''
+     * @dev fully initializes the contract and its parents
+    '''
+    def initialize(self) -> None:
+        self.__PoolTokenFactory_init();
+
+    # solhint-disable func-name-mixedcase
+
+    '''
+     * @dev initializes the contract and its parents
+    '''
+    def __PoolTokenFactory_init(self) -> None:
+        self.__PoolTokenFactory_init_unchained();
+
+    '''
+     * @dev performs contract-specific initialization
+    '''
+    def __PoolTokenFactory_init_unchained(self) -> None:
+        # a mapping between tokens and custom symbol overrides (only needed for tokens with malformed symbol property)
+        self._tokenSymbolOverrides = mapping(lambda: str());
+
+        # a mapping between tokens and custom token overrides (only needed for tokens with malformed decimals property)
+        self._tokenDecimalsOverrides = mapping(lambda: uint8());
+
+    # solhint-enable func-name-mixedcase
+
+    '''
+     * @inheritdoc IPoolTokenFactory
+    '''
+    def tokenSymbolOverride(self, token) -> (str):
+        return self._tokenSymbolOverrides[token];
+
+    '''
+     * @dev sets the custom symbol override for a given reserve token
+     *
+     * requirements:
+     *
+     * - the caller must be the admin of the contract
+    '''
+    def setTokenSymbolOverride(self, token, symbol) -> None:
+        self._tokenSymbolOverrides[token] = symbol;
+
+    '''
+     * @inheritdoc IPoolTokenFactory
+    '''
+    def tokenDecimalsOverride(self, token) -> (uint):
+        return self._tokenDecimalsOverrides[token];
+
+    '''
+     * @dev sets the decimals override for a given reserve token
+     *
+     * requirements:
+     *
+     * - the caller must be the admin of the contract
+    '''
+    def setTokenDecimalsOverride(self, token, decimals) -> None:
+        self._tokenDecimalsOverrides[token] = decimals;
+
+    '''
+     * @inheritdoc IPoolTokenFactory
+    '''
+    def createPoolToken(self, token) -> (PoolToken):
+        customSymbol = self._tokenSymbolOverrides[token];
+        tokenSymbol = customSymbol if len(customSymbol) != 0 else token.symbol();
+
+        customDecimals = self._tokenDecimalsOverrides[token];
+        tokenDecimals = customDecimals if customDecimals != 0 else token.decimals();
+
+        symbol = "".join(self.POOL_TOKEN_SYMBOL_PREFIX, tokenSymbol);
+        name = "".join(self.POOL_TOKEN_NAME_PREFIX, " ", tokenSymbol, " ", self.POOL_TOKEN_NAME_SUFFIX);
+
+        newPoolToken = PoolToken(name, symbol, tokenDecimals, token);
+
+        return newPoolToken;

db9bdf5d2c966f7893237e83bc72d827565d798b
Barak Manos <barakman@yahoo.com>
2022-06-27 19:36:29 +0200

    Fix the `parse` utility function.

--- bancor3_emulator/utils/__init__.py
+++ bancor3_emulator/utils/__init__.py
@@ -11,10 +11,12 @@ def using(libraryHandle, classHandle):
             setattr(classHandle, varName, getattr(libraryHandle, varName))
 
 def parse(cast, var, attr):
-    if inspect.isclass(var):
+    if hasattr(var, attr):
         return cast(getattr(var, attr))
     if type(var) is dict:
         return cast(var[attr])
+    if var is not None:
+        return cast(var)
     return cast(0)
 
 class account:

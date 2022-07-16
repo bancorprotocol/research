db8d0ca3f5418434a7104fc45ab929de2cd45892
Tiago Silva <tfn.da.silva@gmail.com>
2022-07-16 20:20:41 +0100

    Added Simple template for the Main Dashboard
    One component to embed the Tableau Dashboard, support desktop and phone.

---
+++ bancor_research/bancor_analytics/template-dashboard/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MainDashboard from './components/MainDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bancor 3 Dashboard</h1>
+      <MainDashboard />
+    </div>
+  );
+}
+
+export default App;

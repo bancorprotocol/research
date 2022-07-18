5d561615cd04c7d01bf0d3adb62afa81211573e3
mikewcasale <mike.w.casale@gmail.com>
2022-07-17 20:39:47 -0700

    created etl module

--- bancor_research/bancor_analytics/template-old/src/redux/actions/customizerActions.js
+++
@@ -1,4 +0,0 @@
-import { createAction } from 'redux-actions';
-
-export const toggleBoxShadow = createAction('TOGGLE_BOX_SHADOW');
-export const toggleTopNavigation = createAction('TOGGLE_TOP_NAVIGATION');
--- bancor_research/bancor_analytics/theme/js/mrare/plyr.js
+++
@@ -1,9 +0,0 @@
-//
-//
-// plyr.js
-//
-// Initialises the plyr video and audio player
-
-import Plyr from 'bancor_research/bancor_analytics/theme/js/mrare/plyr';
-
-Plyr.setup('[data-provider],.plyr');

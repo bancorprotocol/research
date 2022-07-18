e7f62f4fd443a8a917675917cd70950a749ee1c7
mikewcasale <mike.w.casale@gmail.com>
2022-07-17 20:39:47 -0700

    created etl module

--- bancor_research/bancor_analytics/template-old/src/redux/actions/blocksShadowsActions.js
+++
@@ -1,4 +0,0 @@
-import { createAction } from 'redux-actions';
-
-export const changeBlocksShadowsToOnAction = createAction('CHANGE_BLOCKS_SHADOWS_ON');
-export const changeBlocksShadowsToOffAction = createAction('CHANGE_BLOCKS_SHADOWS_OFF');

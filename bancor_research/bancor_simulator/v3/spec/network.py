84b111babfaf5816b6d9aa24cd89a52fabedfe12
barakman <barakman@yahoo.com>
2022-07-24 15:27:30 +0300

    Extend the simulator's `describe` function to output the protocol balance.

--- bancor_research/bancor_simulator/v3/spec/network.py
+++ bancor_research/bancor_simulator/v3/spec/network.py
@@ -393,7 +393,7 @@ class BancorDapp:
 
     def describe(self, decimals: int = -1):
         """
-        Describes the state ledger balances in a format similar to BIP15 documentation.
+        Describes the state ledger in a format similar to BIP15 documentation.
         """
         table = {}
 
@@ -403,19 +403,22 @@ class BancorDapp:
             table[tkn_name] = {}
 
             for username in state.usernames:
-                table[tkn_name][tuple(['Account', username])] = state.users[username].wallet[tkn_name].balance
+                table[tkn_name][tuple(["Account", username])] = state.users[username].wallet[tkn_name].balance
+
+        for tkn_name in state.whitelisted_tokens:
+            table[       tkn_name][tuple(["Contract", "Master Vault"])] = state.tokens[tkn_name].master_vault.balance
+            table[       tkn_name][tuple(["Contract", "EP Vault"    ])] = state.tokens[tkn_name].external_protection_vault.balance
+            table[       tkn_name][tuple(["Contract", "ER Vault"    ])] = state.tokens[tkn_name].standard_rewards_vault.balance
+            table["bn" + tkn_name][tuple(["Contract", "Protocol"    ])] = state.tokens[tkn_name].protocol_wallet_pooltokens.balance
 
         for tkn_name in [tkn_name for tkn_name in state.whitelisted_tokens if tkn_name != "bnt"]:
-                table[tkn_name][tuple(['Contract', 'Master Vault'            ])] = state.tokens[tkn_name].master_vault.balance
-                table[tkn_name][tuple(['Contract', 'EP Vault'                ])] = state.tokens[tkn_name].external_protection_vault.balance
-                table[tkn_name][tuple(['Contract', 'ER Vault'                ])] = state.tokens[tkn_name].standard_rewards_vault.balance
-                table[tkn_name][tuple(['Pool'    , 'a: TKN Staked Balance'   ])] = state.tokens[tkn_name].staking_ledger.balance
-                table[tkn_name][tuple(['Pool'    , 'b: TKN Trading Liquidity'])] = state.tokens[tkn_name].tkn_trading_liquidity.balance
-                table[tkn_name][tuple(['Pool'    , 'c: BNT Trading Liquidity'])] = state.tokens[tkn_name].bnt_trading_liquidity.balance
-                table[tkn_name][tuple(['Pool'    , 'd: BNT Current Funding'  ])] = state.tokens[tkn_name].bnt_funding_amt.balance
-                table[tkn_name][tuple(['Pool'    , 'e: Spot Rate'            ])] = state.tokens[tkn_name].spot_rate
-                table[tkn_name][tuple(['Pool'    , 'f: Average Rate'         ])] = state.tokens[tkn_name].ema_rate
-                table[tkn_name][tuple(['Pool'    , 'g: Average Inverse Rate' ])] = state.tokens[tkn_name]._inv_ema_rate
+            table[tkn_name][tuple(["Pool", "a: TKN Staked Balance"   ])] = state.tokens[tkn_name].staking_ledger.balance
+            table[tkn_name][tuple(["Pool", "b: TKN Trading Liquidity"])] = state.tokens[tkn_name].tkn_trading_liquidity.balance
+            table[tkn_name][tuple(["Pool", "c: BNT Trading Liquidity"])] = state.tokens[tkn_name].bnt_trading_liquidity.balance
+            table[tkn_name][tuple(["Pool", "d: BNT Current Funding"  ])] = state.tokens[tkn_name].bnt_funding_amt.balance
+            table[tkn_name][tuple(["Pool", "e: Spot Rate"            ])] = state.tokens[tkn_name].spot_rate
+            table[tkn_name][tuple(["Pool", "f: Average Rate"         ])] = state.tokens[tkn_name].ema_rate
+            table[tkn_name][tuple(["Pool", "g: Average Inverse Rate" ])] = state.tokens[tkn_name]._inv_ema_rate
 
         df = pd.DataFrame(table).sort_index()
         return df.applymap(lambda x : round(x, decimals)) if decimals >= 0 else df

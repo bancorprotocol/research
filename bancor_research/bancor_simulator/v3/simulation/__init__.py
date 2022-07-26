ff654588aff69dc16a71bfe223a8432588021475
mikewcasale <mike.w.casale@gmail.com>
2022-07-25 20:37:15 -0700

    update license to MIT in simulator

--- bancor_research/bancor_simulator/v3/simulation/__init__.py
+++ bancor_research/bancor_simulator/v3/simulation/__init__.py
@@ -1,4 +1,4 @@
 # coding=utf-8
 # --------------------------------------------------------------------------------------------------------------------
-# Licensed under the Bprotocol Foundation (Bancor) LICENSE. See License.txt in the project root for license information.
+# Licensed under the MIT LICENSE. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------------------------------
--- bancor_research/bancor_simulator/v3/spec/actions.py
+++ bancor_research/bancor_simulator/v3/spec/actions.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------------------------------------------------------
-# Licensed under the Bprotocol Foundation (Bancor) LICENSE. See License.txt in the project root for license information.
+# Licensed under the MIT LICENSE. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------------------------------
 """Agent-based actions. (e.g. trade, deposit, withdraw, etc...)"""
 from pydantic.dataclasses import dataclass

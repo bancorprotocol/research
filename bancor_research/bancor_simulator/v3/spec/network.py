b932afdaa652c3b3f016f3c145ab0fafe5903ab3
barak manos <>
2022-08-09 18:10:17 +0300

    Fix `BancorDapp` Standard-Rewards API

--- bancor_research/bancor_simulator/v3/spec/network.py
+++ bancor_research/bancor_simulator/v3/spec/network.py
@@ -540,7 +540,6 @@ class BancorDapp:
         rewards_amt: str,
         start_time: int,
         end_time: int,
-        user_name: str,
         timestamp: int,
         transaction_type="create_standard_rewards_program",
     ):
@@ -549,7 +548,7 @@ class BancorDapp:
         """
         state = self.get_state(copy_type="initial", timestamp=timestamp)
         state, tkn_name, user_name = validate_input(
-            state, tkn_name, user_name, timestamp
+            state, tkn_name, "", timestamp
         )
         tkn_amt = Decimal(rewards_amt)
         state = create_standard_reward_program(
@@ -626,7 +625,6 @@ class BancorDapp:
 
     def claim_standard_rewards(
         self,
-        tkn_name: str,
         user_name: str,
         program_ids: List[int],
         timestamp: int = 0,
@@ -637,7 +635,7 @@ class BancorDapp:
         """
         state = self.get_state(copy_type="initial", timestamp=timestamp)
         state, tkn_name, user_name = validate_input(
-            state, tkn_name, user_name, timestamp
+            state, "", user_name, timestamp
         )
         state = claim_standard_rewards(
             state=state,

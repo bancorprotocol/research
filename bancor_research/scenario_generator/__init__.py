4b1cbae08f55d67dbe6aa7d8cc7bda698dd5bd1b
mikewcasale <mike.w.casale@gmail.com>
2022-08-31 14:51:51 -0700

    Update __init__.py
    - fixed an import error

--- bancor_research/scenario_generator/__init__.py
+++ bancor_research/scenario_generator/__init__.py
@@ -2,4 +2,4 @@
 # --------------------------------------------------------------------------------------------------------------------
 # Licensed under the MIT LICENSE. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------------------------------
-from monte_carlo_generator import MonteCarloGenerator
+from .monte_carlo_generator import MonteCarloGenerator

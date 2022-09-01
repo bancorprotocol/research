45ba1c7dfb19f34414a551d7b0a3447c849eace8
mikewcasale <mike.w.casale@gmail.com>
2022-09-01 01:01:41 -0700

    Update __init__.py

--- bancor_research/bancor_simulator/__init__.py
+++ bancor_research/bancor_simulator/__init__.py
@@ -4,15 +4,4 @@
 # --------------------------------------------------------------------------------------------------------------------
 """Spec and agent-based simulation modules for Bancor v3."""
 
-import warnings
-
 import bancor_research.bancor_simulator.v3.spec
-
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore", FutureWarning)
-    warnings.simplefilter("ignore", DeprecationWarning)
-
-warnings.filterwarnings("ignore", category=FutureWarning)
-warnings.filterwarnings("ignore", category=DeprecationWarning)
-
-__version__ = "2.0.2"
